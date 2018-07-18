ff3b6ff868c5bd5d0f73876148dfa5651535f485
unknown <muche>
2018-07-18 21:59:10 +0100

    add build script

--- server/routes/entryRoute.js
+++ server/routes/entryRoute.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import entry from '../controllers/entries';
-import Validate from '../middlewares/validate';
+import validate from '../middlewares/validate';
 
 const router = express.Router();
 
 router.get('/', entry.getAll);
 router.get('/:entryId', entry.getOne);
-router.post('/', Validate.createEntry, entry.createEntry);
-router.put('/:entryId', entry.modifyEntry);
+router.post('/', validate.createEntry, entry.createEntry);
+router.put('/:entryId', validate.modifyEntry, entry.modifyEntry);
 
 
 export default router;
