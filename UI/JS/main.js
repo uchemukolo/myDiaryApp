1e732bf92dd8cd8bbaec0f7e06c6ae13f3c802f6
unknown <muche>
2018-07-19 03:38:25 +0100

    refactor UI template to be more responsive

--- UI/JS/main.js
+++ UI/JS/main.js
@@ -2,16 +2,25 @@ const modal = document.getElementById('myModal');
 const btn = document.getElementById("myBtn");
 const span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+btn.onclick = () => {
     modal.style.display = "block";
 }
 
-span.onclick = function() {
+span.onclick = () => {
     modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+window.onclick = (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+}
+
+function myFunction() {
+    const x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
     }
 }
