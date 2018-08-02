b2d14522ab2efa267688aaecde105635cdbb25a0
unknown <muche>
2018-08-02 04:12:04 +0100

    [bg #159479248] updated package file

--- server/routes/entryRoute.js
+++ server/routes/entryRoute.js
@@ -5,8 +5,8 @@ import auth from '../helpers/authentication';
 
 const router = express.Router();
 
-router.post('/', auth.verify, validate.createEntry, entry.createEntry);
-router.put('/:entryId', auth.verify, validate.entryId, validate.modifyEntry, entry.modifyEntry);
+router.post('/', auth.verify, validate.entry, entry.createEntry);
+router.put('/:entryId', auth.verify, validate.entryId, validate.entry, entry.modifyEntry);
 router.get('/', auth.verify, entry.getAll);
 router.get('/:entryId', auth.verify, validate.entryId, entry.getOne);
 router.delete('/:entryId', auth.verify, validate.entryId, entry.deleteEntry);
