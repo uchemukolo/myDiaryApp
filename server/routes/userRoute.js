85913b61e7e86ccb53f1849b9472fa8453772e4c
unknown <muche>
2018-08-30 22:02:00 +0100

    remove authentication from the reset password route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -12,7 +12,7 @@ router.get('/profile', auth.verify, user.userProfile);
 router.put('/profile/update', auth.verify, user.updateProfile);
 router.post('/reminder', auth.verify, validate.addReminder, user.addReminder);
 router.post('/password/resetLink', user.forgotPassword);
-router.put('/password/change', auth.verify, user.createNewPassword);
+router.put('/password/change', user.createNewPassword);
 
 
 export default router;
