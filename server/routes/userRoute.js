5c83b32b8c423d48113607b9c8b8e54f0f8f5e85
unknown <muche>
2018-08-30 17:29:49 +0100

    [ft #160047572]  add reset password route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -11,6 +11,8 @@ router.post('/login', validate.login, user.signIn);
 router.get('/profile', auth.verify, user.userProfile);
 router.put('/profile/update', auth.verify, user.updateProfile);
 router.post('/reminder', auth.verify, validate.addReminder, user.addReminder);
+router.post('/password/resetLink', user.forgotPassword);
+router.put('/password/change', auth.verify, user.createNewPassword);
 
 
 export default router;
