da8f6eb6402b2f43b55236bd07c9a99518cce430
unknown <muche>
2018-08-30 17:30:35 +0100

    [ft #160047572] add email fuctionality to reset password

---
+++ server/helpers/resetPasswordMailer.js
@@ -0,0 +1,37 @@
+import transporter from './mailer';
+
+
+const Passwordmailer = {
+
+  emailSender: (emailAddress, subject, body) => {
+    const emailOptions = {
+      from: 'My Diary Team',
+      to: emailAddress,
+      subject,
+      html: `<div style="padding: .5em;">${body}</div>`
+    };
+    return transporter.sendMail(emailOptions, (error) => {
+      if (error) {
+        return false;
+      }
+      return true;
+    });
+  },
+  passwordResetEmail: (username, id, token, email) => {
+    const subject = 'New Password reset link';
+    const message = `<p>Dear ${username},
+    </p> <p>kindly click on
+    <a href="https://uchemukolo.github.io/myDiaryApp/public/reset-password.html?i=${id}&t=${token}">Reset password</a>
+    to create a new password</p>
+    <p><strong>CHEERS!</strong></p>`;
+    Passwordmailer.emailSender(email, subject, message);
+  },
+  passwordChangeEmail: (username, email) => {
+    const subject = 'Password successfully changed';
+    const message = `<p>Dear <b>${username}</b>,
+    <p>Your password has been successfully changed, kindly login with your new Password.</p>;
+    <p><strong>CHEERS!</strong></p>`;
+    Passwordmailer.emailSender(email, subject, message);
+  }
+};
+export default Passwordmailer;
